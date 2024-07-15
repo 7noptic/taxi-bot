e354948ce8ffb76c0136706c8725a6a4428516f4
7noptic <7noptic@gmail.com>
2024-07-16 00:35:52 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
 	app.enableCors({
-		origin: 'http://taxi-bot-admin.ru',
+		origin: true,
 	});
 	setupGracefulShutdown({ app });
 	await app.listen(3000);
